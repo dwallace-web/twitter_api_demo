2894f0db1e7596f21830f105318948e3771f1f03
David Wallace <dwallacemarketing@gmail.com>
2022-06-20 21:57:10 -0500

    pull recent tweets

--- get_tweets.py
+++ get_tweets.py
@@ -10,30 +10,44 @@ current_date = str(date.today())
 # SIMPLE STRUCTURE
 my_name = 'dtheblerd'
 client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
-user = client.get_user(username=my_name)
+self1 = client.get_user(username=my_name)
 auth = tweepy.OAuthHandler(consumer_key=config.API_KEY,
                            consumer_secret=config.API_KEY_SECRET)
 api = tweepy.API(auth)
-user2 = api.get_user(screen_name=my_name)
+self2 = api.get_user(screen_name=my_name)
 
-print("% s has % s followers" % (user.data, user2.followers_count))
+print("% s has % s followers" % (self1.data, self2.followers_count))
 
 # How to write a query #dev docs -- https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
 # How to write an academic query -- https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academic-research
 
-query = 'AEW -is:retweet -is:reply is:verified'
+query = 'AEW -is:retweet -is:reply is:verified -is:reply is:verified'
 
-Response = client.search_recent_tweets(
-    query=query, max_results=10, tweet_fields=['lang', 'created_at', 'public_metrics', 'source'])
-print(Response)
-# print(Response.data)
+res = client.search_recent_tweets(
+    query=query,
+    max_results=10,
+    tweet_fields=['lang',
+                  'created_at',
+                  'public_metrics',
+                  'source',
+                  'id',
+                  'author_id',
+                  'referenced_tweets',
+                  'conversation_id'
+                  ],
+    expansions=['author_id'],
+    user_fields=['username', 'id', 'description', 'name']
+)
+# print(res)
+# print(res.includes['users'])
 
-for tweet in Response.data:
-    print(tweet.text)
-    print(tweet.lang)
-    print(tweet.created_at)
-    print(tweet.public_metrics)
-    print(tweet.source)
+users = {u['id']: u for u in res.includes['users']}
 
-print('wait')
+for tweet in res.data:
+    if users[tweet.author_id]:
+        user = users[tweet.author_id]
+        print("This tweet is from % s using the % s"
+              % (user.username, tweet.source))
+        print(tweet.text)
+
 print('review')
