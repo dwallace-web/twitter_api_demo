1228bd8eb1bd9242014e0d32750a1835db1d0d25
David Wallace <David@David-Macbook-Air.local>
2022-11-15 20:51:29 -0600

    scrape additional fields, organize and print into .csv file

--- get_following.py
+++ get_following.py
@@ -1,4 +1,3 @@
-import datetime
 import time
 import tweepy
 from tweepy.api import pagination
@@ -7,23 +6,38 @@ import config
 import csv
 from datetime import date
 from datetime import datetime
-import os 
 
 # date
 collection = str(date.today())
 
+start_time = str(datetime.now())
+print(f'Scrape started at {start_time}')
+
 # SIMPLE STRUCTURE
 
 Client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
-auth = tweepy.OAuthHandler(consumer_key=config.API_KEY,
-                           consumer_secret=config.API_KEY_SECRET)
-api = tweepy.API(auth)
+# auth = tweepy.OAuthHandler(consumer_key=config.API_KEY,consumer_secret=config.API_KEY_SECRET)
+# api = tweepy.API(auth)
 
-# user = Client.get_user(username=config.MY_USERNAME)
+user = Client.get_user(username=config.MY_USERNAME)
 # user2 = api.get_user(screen_name=config.MY_USERNAME)
 
-res = Client.get_users_following(id=user2.id,max_results=1000)
+res = Client.get_users_following( 
+    id=user[0].id,
+    max_results=1000,
+    user_fields=[
+    'public_metrics'
+    ,'protected'
+    ,'verified'
+    ,'location'
+    ,'description'
+    ,'created_at'
+    ,'withheld'
+    ]
+)
+
 following = res.data
+following.reverse()
 
 date_tweeted = ''
 tweet_text = ''
@@ -32,40 +46,55 @@ follower_count = 0
 with open(f"data_following_{collection}.csv", 'a', newline="") as csvfile:
     writer = csv.writer(csvfile)
 
+    #header 
+    user_info = [
+        'collection date',
+        'customer id',
+        'username',
+        'verified',
+        'bio',
+        'display name',
+        'tweet date',
+        'tweet body',
+        'following count',
+        'follower count'
+    ]
+    writer.writerow(user_info)
+
+    #build final csv
     for i in following:
         
-        recent_tweets = Client.get_users_tweets(i.id, max_results=5,tweet_fields=['created_at']).data
-        # print the follower count for each member
-        recent_user_data = api.get_user(user_id=str(i.id))
+        get_recent_tweets = Client.get_users_tweets(i.id, max_results=5,tweet_fields=['created_at','public_metrics']).data
 
-        if recent_tweets is not None:
-            date_tweeted = recent_tweets[0].created_at
+        if get_recent_tweets is not None:
+            date_tweeted = get_recent_tweets[0].created_at
         else:
             date_tweeted = "Unknown Date"
         
-        if recent_tweets is not None:
-            tweet_text = recent_tweets[0].text
+        if get_recent_tweets is not None:
+            tweet_text = get_recent_tweets[0].text
         else:
             tweet_text = "Unknown Tweet"
-
-        if recent_user_data is not None:
-            follower_count = recent_user_data.followers_count
-        else:
-            follower_count = "Unknown Follower Count"
 
         following_info = [
         collection,
         i.id,
         i.username,
+        i.verified,
+        i.description,
         i.name,
         date_tweeted,
         tweet_text,
-        follower_count 
+        i.public_metrics['following_count'],
+        i.public_metrics['followers_count']
         ]  
 
         writer.writerow(following_info)
 
-        print(f'{i.username} is complete, next!')
-        time.sleep(4)
+        # print(f'@{i.username} is complete, next!')
+        time.sleep(5)
 
 csvfile.close()
+
+end_time = str(datetime.now())
+print(f'Scrape ended at {end_time}')
