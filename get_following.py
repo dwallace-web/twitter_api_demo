a01582cc4f8c877a271a50b57231557fb19660d5
David Wallace <David@David-Macbook-Air.local>
2022-11-23 00:44:11 -0600

    👨🏾‍💻

--- get_following.py
+++ get_following.py
@@ -5,15 +5,14 @@ from datetime import date, datetime
 
 # date
 collection = str(date.today())
-start_time = str(datetime.now())
-print(f'Scrape started at {start_time}')
-
-# SIMPLE STRUCTURE
+print(f'Scrape started at {str(datetime.now())}')
+following = []
+count = 0
+total_users = len(following)
+data_for_csv = []
 
 Client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
 user = Client.get_user(username=config.MY_USERNAME)
-# user2 = api.get_user(screen_name=config.MY_USERNAME)
-
 res = Client.get_users_following( 
     id=user[0].id,
     max_results=1000,
@@ -27,98 +26,123 @@ res = Client.get_users_following(
     ,'withheld'
     ,'url'
     ]
+    # ,tweet_fields=['created_at','public_metrics','context_annotations','geo']
 )
 
-following = res.data
-following.reverse()
+def fetch_data(x):
+    fetch = []
+    fetch = Client.get_users_tweets(x.id, max_results=5,tweet_fields=['created_at','public_metrics','context_annotations','geo'],exclude=['retweets'])
+    return fetch 
 
-count = 0
-
-total_users = len(following)
-data_for_csv = []
-retain_api_data = []
-
-
-def fetch_users():
-    global count
-
+def clean_data(user, input):
+    output = {}
     date_tweeted = ''
     tweet_text = ''
-    follower_count = 0
 
-    for i in following:
-            if count <= 3: 
-                fetch = Client.get_users_tweets(i.id, max_results=100,tweet_fields=['created_at','public_metrics'])
+    if user.protected is True:
+        date_tweeted = "Unknown Date"
+        tweet_text = "Unknown Tweet"
+    elif len(input) == 0:
+        date_tweeted = "Not Applicable - Public Account with no tweets."
+        tweet_text = "Not Applicable - Public Account with no tweets."
+    else:
+        date_tweeted = input[0].created_at
+        tweet_text = input[0].text
 
-                get_recent_tweets = fetch.data
-                
-                if i.protected is True:
-                    date_tweeted = "Unknown Date"
-                    tweet_text = "Unknown Tweet"
-                else:
-                    tweet_text = get_recent_tweets[0].text
-                    date_tweeted = get_recent_tweets[0].created_at
+    output = {
+        'date_tweeted': date_tweeted,
+        'tweet_text': tweet_text
+    }
 
-                following_info = [
-                collection,
-                i.id,
-                i.username,
-                i.verified,
-                i.location,
-                i.description,
-                i.name,
-                date_tweeted,
-                tweet_text,
-                i.public_metrics['following_count'],
-                i.public_metrics['followers_count'],
-                i.public_metrics['tweet_count'],
-                i.url
-                ]  
+    return output 
 
-                data_for_csv.append(following_info)
+def notify_admin(a,b,c):
+    user_time = str(datetime.now())
+    print(f'At {user_time} @{a} user {b} of {c} was completed, next!')
 
-                count+=1
-                user_time = str(datetime.now())
-                print(f'At {user_time} @{i.username} user {count} of {total_users} was completed, next!')
-                # time.sleep(1)
-            else:
-                continue
-    
-    end_time = str(datetime.now())
-    print(f'Scrape ended at {end_time}')
-    write_to_file()
+def get_stats(x):
+    total_users = len(x.data)
+    total_unverified = 0
+    total_verified = 0
+    total_private = 0 
+
+    for a in x.data:
+        if a.verified == False:
+            total_unverified+=1
+    print(f"Total unverified population {total_unverified}")
+
+    for b in x.data:
+        if b.verified == True:
+            total_verified+=1
+    print(f"Total verified population {total_verified}")
+
+    for c in x.data:
+        if c.protected == True:
+            total_private+=1
+    print(f"Total private population {total_private}")
+
+    return total_users
 
 def write_to_file():
     with open(f'data_following_{collection}.csv', 'a', newline="") as csvfile:
         writer = csv.writer(csvfile)
-
         #header 
         user_info = [
             'collection date',
             'customer id',
             'username',
+            'private',
             'verified',
             'location',
             'bio',
             'display name',
-            'tweet date',
-            'tweet body',
             'following count',
             'follower count',
             'total tweets',
-            'outbound link'
+            'outbound link',
+            'recent tweet date',
+            'recent tweet body',
         ]
         writer.writerow(user_info)
         for i in data_for_csv:
             writer.writerow(i)
     csvfile.close()
 
-def store_everything():
-    with open(f"store_everything_{collection}.csv", 'a', newline="") as csv_2:
-        writer = csv.writer(csv_2)
-        for i in retain_api_data:
-            writer.writerow(retain_api_data)
-    csv_2.close()
 
-fetch_users()
-# store_everything()
+def handle_users(x):
+    global count
+
+    total_users = get_stats(x)
+
+    for i in x.data:
+        time.sleep(4)     
+        count+=1
+        if i.verified == False: 
+            # get_recent_tweets = fetch_data(i)
+            # filtered_data = clean_data(i, get_recent_tweets.data)
+
+            following_info = [
+            collection,
+            i.id,
+            i.username,
+            i.protected,
+            i.verified,
+            i.location,
+            i.description,
+            i.name,
+            i.public_metrics['following_count'],
+            i.public_metrics['followers_count'],
+            i.public_metrics['tweet_count'],
+            i.url,
+            # filtered_data['date_tweeted'],
+            # filtered_data['tweet_text'],
+            ]  
+
+            data_for_csv.append(following_info)
+            notify_admin(i.username, count, total_users)
+        else:
+            continue
+    print(f'Scrape ended at {str(datetime.now())}')
+    write_to_file()
+
+handle_users(res)
