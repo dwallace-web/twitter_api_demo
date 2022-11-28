eaa7cb2c44162799d50467a8c24f3fd1144b07a6
David Wallace <David@David-Macbook-Air.local>
2022-11-27 21:24:52 -0600

    updates

--- get_following.py
+++ get_following.py
@@ -1,33 +1,95 @@
 import time, tweepy, config, csv, os, requests
-from tweepy.api import pagination
-from tweepy.client import Response
+import tweepy
+
 from datetime import date, datetime
 
 # date
 collection = str(date.today())
 print(f'Scrape started at {str(datetime.now())}')
-following = []
 count = 0
-total_users = len(following)
 data_for_csv = []
+res = []
 
 Client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
-user = Client.get_user(username=config.MY_USERNAME)
-res = Client.get_users_following( 
-    id=user[0].id,
-    max_results=1000,
-    user_fields=[
-    'public_metrics'
-    ,'protected'
-    ,'verified'
-    ,'location'
-    ,'description'
-    ,'created_at'
-    ,'withheld'
-    ,'url'
-    ]
-    # ,tweet_fields=['created_at','public_metrics','context_annotations','geo']
-)
+user = Client.get_user(username=config.USERNAME)
+
+def fetch_users():
+    global res
+
+    for response in tweepy.Paginator(Client.get_users_following, id=user[0].id,
+        max_results=1000,
+        user_fields=[
+        'public_metrics'
+        ,'protected'
+        ,'verified'
+        ,'location'
+        ,'description'
+        ,'created_at'
+        ,'withheld'
+        ,'url'
+        ]):
+        res.extend(response.data)
+        print(response.meta)
+
+# res_og = Client.get_users_following( 
+#     id=user[0].id,
+#     max_results=1000,
+#     user_fields=[
+#     'public_metrics'
+#     ,'protected'
+#     ,'verified'
+#     ,'location'
+#     ,'description'
+#     ,'created_at'
+#     ,'withheld'
+#     ,'url'
+#     ]
+#     # ,tweet_fields=['created_at','public_metrics','context_annotations','geo']
+# )
+
+def handle_users(x):
+    global count
+
+    date_tweeted = ''
+    tweet_text = ''
+
+    # total_users = get_stats(x)
+
+    for i in x:
+        count+=1
+        if i.public_metrics['following_count'] >= 0:
+            # time.sleep(3)     
+            # get_recent_tweets = fetch_data(i)
+            # filtered_data = clean_data(i, get_recent_tweets.data)
+            # date_tweeted = filtered_data['date_tweeted']
+            # tweet_text = filtered_data['tweet_text']
+
+            following_info = [
+            collection,
+            i.id,
+            i.username,
+            i.protected,
+            i.verified,
+            i.location,
+            i.description,
+            i.name,
+            i.public_metrics['following_count'],
+            i.public_metrics['followers_count'],
+            i.public_metrics['tweet_count'],
+            i.url,
+            date_tweeted,
+            tweet_text
+            ]  
+
+            data_for_csv.append(following_info)
+            # notify_admin(i.username, count, total_users)
+        else:
+            continue
+    print(f'Scrape ended at {str(datetime.now())}')
+    write_to_file()
+    print('Done')
+
+
 
 def fetch_data(x):
     fetch = []
@@ -42,7 +104,10 @@ def clean_data(user, input):
     if user.protected is True:
         date_tweeted = "Unknown Date"
         tweet_text = "Unknown Tweet"
-    elif len(input) == 0:
+    elif input is None:
+        date_tweeted = "Not Applicable - Public Account with no tweets."
+        tweet_text = "Not Applicable - Public Account with no tweets."
+    elif len(input) == 0 or None:
         date_tweeted = "Not Applicable - Public Account with no tweets."
         tweet_text = "Not Applicable - Public Account with no tweets."
     else:
@@ -108,41 +173,5 @@ def write_to_file():
             writer.writerow(i)
     csvfile.close()
 
-
-def handle_users(x):
-    global count
-
-    total_users = get_stats(x)
-
-    for i in x.data:
-        time.sleep(4)     
-        count+=1
-        if i.verified == False: 
-            # get_recent_tweets = fetch_data(i)
-            # filtered_data = clean_data(i, get_recent_tweets.data)
-
-            following_info = [
-            collection,
-            i.id,
-            i.username,
-            i.protected,
-            i.verified,
-            i.location,
-            i.description,
-            i.name,
-            i.public_metrics['following_count'],
-            i.public_metrics['followers_count'],
-            i.public_metrics['tweet_count'],
-            i.url,
-            # filtered_data['date_tweeted'],
-            # filtered_data['tweet_text'],
-            ]  
-
-            data_for_csv.append(following_info)
-            notify_admin(i.username, count, total_users)
-        else:
-            continue
-    print(f'Scrape ended at {str(datetime.now())}')
-    write_to_file()
-
+fetch_users()
 handle_users(res)
